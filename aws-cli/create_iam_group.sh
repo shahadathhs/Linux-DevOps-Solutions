29b0f96593a2e51f0f9d3a592832b306ce04fcef
Sleety Matt George <70995581+sleetymattgeorge@users.noreply.github.com>
2025-04-24 14:25:09 +0530

    to create iam_group in aws identity management centre

---
+++ aws-cli/create_iam_group.sh
@@ -0,0 +1,2 @@
+aws identitystore list-groups --identity-store-id <check_iam_center_settings> --profile <credentials_profile_name> --region <eks_region>
+aws identitystore create-group   --identity-store-id <check_iam_center_settings>   --display-name "<group_name>"   --description "<describe what the group is for"   --region <eks_region>   --profile <credentials_profile_name>
